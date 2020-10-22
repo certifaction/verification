bbe91083a298c3d16280268eafc8fd0a459290f8
Cyrill Steinmetz <cyrill.steinmetz@certifaction.io>
2020-10-22 12:01:31 +0200

    BP-2349: base layout (#23)

---
+++ packages/verification-vue-component/src/components/Verification/items/cards/BaseCard.vue
@@ -0,0 +1,28 @@
+<template>
+    <div class="base-card">
+        <div class="card-header">
+            <slot name="header"></slot>
+        </div>
+        <div class="card-body">
+            <slot name="body"></slot>
+        </div>
+        <div class="card-footer">
+            <slot name="footer"></slot>
+        </div>
+    </div>
+</template>
+
+<script>
+
+export default {
+    name: 'BaseCard',
+    components: {},
+    props: {
+        expanded: {
+            type: Boolean,
+            default: false
+        }
+    },
+    computed: {}
+}
+</script>
