f3774b7f286111f729adcb599312c09b7279ef72
Cyrill Steinmetz <cyrill.steinmetz@certifaction.io>
2020-10-22 12:01:31 +0200

    BP-2349: base layout (#23)

---
+++ packages/verification-vue-component/src/components/Verification/items/ResultDetail.vue
@@ -0,0 +1,43 @@
+<template>
+    <div class="result-detail">
+        <div class="detail-header">
+            <div class="header-icon">
+                <MDIcon :icon="mdiShieldCheck" class="icon-verified"/>
+            </div>
+            <div class="header-label"></div>
+            <div class="header-action">
+                <button type="button"
+                        class="btn-link advanced-toggler">
+                    <MDIcon :icon="mdiChevronUp" class="toggler"/>
+                </button>
+            </div>
+        </div>
+        <div class="detail-body"></div>
+    </div>
+</template>
+
+<script>
+
+import { mdiShieldCheck, mdiChevronUp } from '@mdi/js'
+import MDIcon from '../../MDIcon.vue'
+
+export default {
+    name: 'ResultDetail',
+    components: {
+        MDIcon
+    },
+    props: {
+        expanded: {
+            type: Boolean,
+            default: false
+        }
+    },
+    data() {
+        return {
+            mdiShieldCheck,
+            mdiChevronUp
+        }
+    },
+    computed: {}
+}
+</script>
