c746672a090f19bac78e084b62e908e2941b4d71
Alessandro Nicoli <397457+alnicoli@users.noreply.github.com>
2020-10-26 17:32:16 +0100

    BP-2350, BP-2355: Verified, unverified card (#26)
    * BP-2350, BP-2355: Verified, unverified card
    
    * BP-2350, BP-2355: Added smart contract to expert info
    
    * BP-2350, BP-2355: Header clickable instead of just button
    
    * BP-2350, BP-2355: Added missing net definition

---
+++ packages/verification-vue-component/src/components/Verification/items/cards/ShadowCard.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="base-card verification-shadow-card">
+        <div class="lines">
+            <div class="line short"></div>
+            <div class="divider"></div>
+            <div class="line medium"></div>
+            <div class="line long"></div>
+            <div class="line long"></div>
+            <div class="line short"></div>
+            <div class="line medium"></div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'ShadowItem'
+}
+</script>
