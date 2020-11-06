8592f4dbf0229fc9e3ec7de39e96d893faa493dc
Alessandro Nicoli <397457+alnicoli@users.noreply.github.com>
2020-11-06 12:03:31 +0100

    BP-2441: Fullpage dropdown (#34)
    * BP-2441: full page drop area
    
    * BP-2441: Split file selector and drop box
    
    * BP-2441: Added missing files
    
    * BP-2441: Various improvments from PR
    
    * BP-2441: cleanup
    
    * BP-2441: scss improvement, fix flickering
    
    * BP-2441: none pointer events on .verification-dropbox

--- packages/verification-vue-component/src/components/Verification/VerificationDropBox.vue
+++ packages/verification-vue-component/src/components/Verification/VerificationDropBox.vue
@@ -1,53 +1,33 @@
 <template>
-    <div class="verification-dropbox" :class="{dragover:draggingOver}"
-         @dragover.prevent="draggingOver = true"
-         @dragleave="draggingOver = false"
-         @drop="handleDrop">
+    <div class="verification-dropbox">
         <div class="dropbox">
             <div class="content">
-                <img src="../../assets/img/dropbox_document.svg" alt="Certifaction"/>
                 <div class="labels">
                     <div class="label title">{{ titleLabel }}</div>
-                    <div class="label subtitle">{{ _$t('verification.dropbox.subtitle') }}</div>
+                    <div class="label subtitle">{{ _$t('verification.fileSelection.subtitle.dropbox') }}</div>
                 </div>
+                <img src="../../assets/img/dropbox_document.svg" alt="Certifaction"/>
             </div>
-            <input class="input-file"
-                   type="file"
-                   multiple
-                   name="uploadField"
-                   accept="application/pdf"
-                   @change="filesDropped($event.target.name, $event.target.files)">
         </div>
     </div>
 </template>
 
 <script>
 import i18nWrapperMixin from '../../mixins/i18n-wrapper'
-
 export default {
     name: 'VerificationDropBox',
     mixins: [
         i18nWrapperMixin
     ],
-    data() {
-        return {
-            draggingOver: false,
-            firstVerification: true
+    props: {
+        firstVerification: {
+            type: Boolean,
+            default: true
         }
     },
     computed: {
         titleLabel() {
-            return this.firstVerification ? this._$t('verification.dropbox.title.first') : this._$t('verification.dropbox.title.following')
-        }
-    },
-    methods: {
-        handleDrop() {
-            this.draggingOver = false
-            this.firstVerification = false
-            this.$emit('drop')
-        },
-        filesDropped(target, files) {
-            this.$emit('files-dropped', files)
+            return this.firstVerification ? this._$t('verification.fileSelection.title.first') : this._$t('verification.fileSelection.title.following')
         }
     }
 }
