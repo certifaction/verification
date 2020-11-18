2f7eb4924acd279ab0885141e05f494513d74761
Alessandro Nicoli <397457+alnicoli@users.noreply.github.com>
2020-11-18 17:07:57 +0100

    BP-2356: one signer rebased (#50)
    * BP-2356: One sign with new structure
    
    * BP-2356: registering item FOR DEMO purposes (to be deleted again)
    
    * BP-2356: Updated translation files
    
    * BP-2356: Fixed file structure
    
    * BP-2356: error and not found fixes
    
    * BP-2356: fixed typo
    
    * BP-2356: removed accidently commited changes
    
    * BP-2356: Improved code, removed unused
    
    * BP-2356: Removed demo code and files
    
    * BP-2356: clear timeouts and improvements
    
    * Fix offchain revoke
    
    * BP-2356: Revocation and offchain fixes
    
    * BP-2356: Simplifed timeout management
    
    * BP-2356: window.clearTimeout
    
    Co-authored-by: Cyrill Steinmetz <cyrill.steinmetz@certifaction.io>

--- packages/verification-vue-component/src/components/Verification/items/VerificationItem.vue
+++ packages/verification-vue-component/src/components/Verification/items/VerificationItem.vue
@@ -2,13 +2,17 @@
     <div class="item-container">
         <ShadowCard v-if="isLoading" />
         <FaqCard v-else-if="showFaq" @toggle-help="toggleHelp($event)" />
-        <ContactCard v-else-if="showContact" @toggle-help="toggleHelp($event)" :certifaction-api-url="certifactionApiUrl" />
-        <VerificationCard v-else :verification-item="verificationItem" @toggle-help="toggleHelp($event)" />
+        <ContactCard v-else-if="showContact" @toggle-help="toggleHelp($event)" :certifaction-api-url="verifierInformation.certifactionApiUrl" />
+        <template v-else>
+            <SigningCard v-if="isSigning" :verification-item="verificationItem" :net="verifierInformation.net" @toggle-help="toggleHelp($event)" />
+            <VerificationCard v-else :verification-item="verificationItem" :net="verifierInformation.net" @toggle-help="toggleHelp($event)" />
+        </template>
     </div>
 </template>
 
 <script>
 import VerificationCard from './cards/VerificationCard.vue'
+import SigningCard from './cards/SigningCard.vue'
 import ShadowCard from './cards/ShadowCard.vue'
 import FaqCard from './cards/FaqCard.vue'
 import ContactCard from './cards/ContactCard.vue'
@@ -17,6 +21,7 @@ export default {
     name: 'VerificationItem',
     components: {
         VerificationCard,
+        SigningCard,
         ShadowCard,
         FaqCard,
         ContactCard
@@ -32,14 +37,17 @@ export default {
             type: Object,
             required: true
         },
-        certifactionApiUrl: {
-            type: String,
+        verifierInformation: {
+            type: Object,
             required: true
         }
     },
     computed: {
         isLoading() {
             return this.verificationItem.hashed === undefined || this.verificationItem.hashed === false
+        },
+        isSigning() {
+            return this.verificationItem.events ? (this.verificationItem.events.filter(event => event.scope === 'sign')).length !== 0 : false
         }
     },
     methods: {
