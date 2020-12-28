ba59ea23cdf0edd92a8996a0df70635af64eddb4
Alessandro Nicoli <alessandro.nicoli@certifaction.io>
2020-12-28 10:38:03 +0100

    BP-2431: removed unused file

--- packages/verification-vue-component/src/utils/pdfjs.js
+++
@@ -1,6 +0,0 @@
-const pdfjs = require('pdfjs-dist')
-pdfjs.GlobalWorkerOptions.workerSrc = require('pdfjs-dist/build/pdf.worker.entry.js')
-
-export default function(src) {
-    return pdfjs.getDocument('https://raw.githubusercontent.com/mozilla/pdf.js/ba2edeae/examples/learning/helloworld.pdf')
-}
