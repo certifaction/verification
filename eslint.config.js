02f86fafc720bbb6155de5bce5ddea062fd1b3d0
bauer00 <3669176+bauer00@users.noreply.github.com>
2025-10-01 13:31:49 +0200

    DT-1627: dependency updates 2025-10 (#220)

--- eslint.config.js
+++ eslint.config.js
@@ -1,20 +1,40 @@
+import { defineConfig } from 'eslint/config'
+import globals from 'globals'
+import js from '@eslint/js'
 import pluginVue from 'eslint-plugin-vue'
-import pluginJest from 'eslint-plugin-jest'
 import skipPrettierFormatting from '@vue/eslint-config-prettier/skip-formatting'
 
-export default [
+export default defineConfig([
     {
         name: 'certifaction/verification/files-to-lint',
-        files: ['**/*.{js,mjs,vue}'],
+        files: ['**/*.{js,mjs,jsx,vue}'],
     },
+
+    {
+        languageOptions: {
+            globals: {
+                ...globals.browser,
+            },
+        },
+    },
+
+    js.configs.recommended,
 
     ...pluginVue.configs['flat/vue2-essential'],
 
     {
         name: 'certifaction/verification/jest-test',
         files: ['**/tests/**', '**/*.test.js'],
-        plugins: { jest: pluginJest },
+        languageOptions: {
+            globals: {
+                ...globals.jest,
+            },
+        },
+        rules: {
+            'no-unused-vars': 'warn',
+            'no-undef': 'warn',
+        },
     },
 
     skipPrettierFormatting,
-]
+])
